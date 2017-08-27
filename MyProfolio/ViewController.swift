4208db299a79c00fd23ed605109a6f60b52a5054
Jack Wong <jackwong@JacknoMacBook-Air.local>
2017-08-27 17:06:35 +0900

    Initial Commit

---
+++ MyProfolio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyProfolio
+//
+//  Created by Jack Wong on 2017/08/27.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
