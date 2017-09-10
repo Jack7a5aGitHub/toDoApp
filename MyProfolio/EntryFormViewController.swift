8b0301a9f0aae8ffcb3920b6a33606b9c453ad4f
Jack7a5aGitHub <jack7a5a1993@icloud.com>
2017-09-10 14:15:33 +0900

    Revert "Revert "9/10 updated version""
    This reverts commit 53d654a182cfd98d20e237983706dcbc32bc0766.

--- MyProfolio/EntryFormViewController.swift
+++ MyProfolio/EntryFormViewController.swift
@@ -13,7 +13,6 @@ class EntryFormViewController: FormViewController {
     
     
     @IBAction func saveBtn(_ sender: Any) {
-<<<<<<< HEAD
         
 <<<<<<< HEAD
         
@@ -24,38 +23,26 @@ class EntryFormViewController: FormViewController {
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let task = Form(context: context)
 >>>>>>> parent of e97babf... 9/9 update before doing the model
-=======
-       
-       
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        let task = Form(context: context)
-       
->>>>>>> parent of 50afe23... 9/10 updated version
         task.task = taskSubject
         task.dueDate = date as NSDate
         task.priority = priority
         task.details = details
         
-        print("success")
-        // save the data 
+        print("save success")
+        // save the data
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController!.popViewController(animated: true)
     }
     
 <<<<<<< HEAD
-<<<<<<< HEAD
     var theChosen  : Form? = nil
 =======
 >>>>>>> parent of e97babf... 9/9 update before doing the model
-=======
-    
->>>>>>> parent of 50afe23... 9/10 updated version
     var taskSubject = String()
     var date = Date()
     var details = String()
     var priority = String()
     
-<<<<<<< HEAD
 <<<<<<< HEAD
     
     override func viewWillAppear(_ animated: Bool) {
@@ -63,11 +50,23 @@ class EntryFormViewController: FormViewController {
 =======
     override func viewDidLoad() {
 >>>>>>> parent of e97babf... 9/9 update before doing the model
-=======
-    override func viewWillAppear(_ animated: Bool) {
-   
->>>>>>> parent of 50afe23... 9/10 updated version
         super.viewDidLoad()
+        
+        
+        if theChosen != nil {
+            print("yes taskSubject")
+            print(theChosen)
+            
+            self.taskSubject = (theChosen?.task)!
+            self.dueDate = (theChosen?.dueDate)! as Date
+            self.priority = (theChosen?.priority)!
+            self.details = (theChosen?.details)!
+
+        }
+
+        
+        
+        
         
         navigationOptions = RowNavigationOptions.Enabled.union(.StopDisabledRow)
         animateScroll = true
@@ -92,12 +91,8 @@ class EntryFormViewController: FormViewController {
                 $0.onChange{[unowned self] row in
                     self.taskSubject = row.value!
                 }
-<<<<<<< HEAD
                 
 <<<<<<< HEAD
-=======
-             
->>>>>>> parent of 50afe23... 9/10 updated version
                 
         }
         form +++ Section()
@@ -117,7 +112,6 @@ class EntryFormViewController: FormViewController {
                 $0.title = "Priority"
                 $0.options = ["L", "M", "H"]
                 $0.value = priority
-<<<<<<< HEAD
                 $0.onChange({[unowned self] row in
                     if let option = row.value{
                         self.priority = option
@@ -133,59 +127,20 @@ class EntryFormViewController: FormViewController {
                 $0.minimumDate = Date(timeIntervalSinceNow: 0)
                 $0.value = date as Date
 >>>>>>> parent of e97babf... 9/9 update before doing the model
-=======
->>>>>>> parent of 50afe23... 9/10 updated version
             }
             +++ Section()
             <<< TextAreaRow(){
                 $0.placeholder = "Details"
                 $0.value = details
-                
-                
-        }
-
-       
-    }
-    /*
-    func setupForm(){
-        
-        form +++ Section("ABC")
-            <<< TextRow(){
-                $0.title = " Task "
-                $0.placeholder = "e.g Interview"
-                $0.value = taskSubject
-                $0.onChange{[unowned self] row in
-                    self.taskSubject = row.value!
-                }
-                
-        }
-        form +++ Section()
-            <<< DateTimeRow(){
-               
-                $0.title = "Due Date"
-                $0.dateFormatter = type(of: self).dateFormatter
-                $0.minimumDate = Date()
-                $0.value = dueDate
                 $0.onChange({[unowned self] row in
-                    if let date = row.value{
-                        self.dueDate = date
-                        
+                    if let typedDetails = row.value{
+                        self.details = typedDetails
                     }})
-            }
-            <<< SegmentedRow<String>() {
-                $0.title = "Priority"
-                $0.options = ["L", "M", "H"]
-                $0.value = priority
-            }
-            +++ Section()
-            <<< TextAreaRow(){
-                $0.placeholder = "Details"
-                $0.value = details
-                
                 
         }
+        
+        
     }
-<<<<<<< HEAD
 <<<<<<< HEAD
     /*
      func setupForm(){
@@ -229,25 +184,16 @@ class EntryFormViewController: FormViewController {
      */
     
     static let dateFormatter: DateFormatter = {
-=======
-    */
-  
-   static let dateFormatter: DateFormatter = {
->>>>>>> parent of 50afe23... 9/10 updated version
         let formatter = DateFormatter()
         formatter.dateFormat = "MMM d yyyy, h mm:a"
         return formatter
     }()
     
-<<<<<<< HEAD
     
 =======
     
     
     
 >>>>>>> parent of e97babf... 9/9 update before doing the model
-=======
- 
->>>>>>> parent of 50afe23... 9/10 updated version
     
 }
