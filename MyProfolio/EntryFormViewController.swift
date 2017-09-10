50afe230b18921c5a5ba674a6ac13f09ebe05ed9
Jack7a5aGitHub <jack7a5a1993@icloud.com>
2017-09-10 14:01:22 +0900

    9/10 updated version

--- MyProfolio/EntryFormViewController.swift
+++ MyProfolio/EntryFormViewController.swift
@@ -13,32 +13,49 @@ class EntryFormViewController: FormViewController {
     
     
     @IBAction func saveBtn(_ sender: Any) {
-       
-       
+        
+        
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let task = Form(context: context)
-       
+        
         task.task = taskSubject
         task.dueDate = dueDate as Date as NSDate
         task.priority = priority
         task.details = details
         
-        print("success")
-        // save the data 
+        print("save success")
+        // save the data
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController!.popViewController(animated: true)
         
     }
     
-    
+    var theChosen  : Form? = nil
     var taskSubject = String()
     var dueDate = Date()
     var details = String()
     var priority = String()
     
+    
     override func viewWillAppear(_ animated: Bool) {
-   
+        print("viewWillAppear")
         super.viewDidLoad()
+        
+        
+        if theChosen != nil {
+            print("yes taskSubject")
+            print(theChosen)
+            
+            self.taskSubject = (theChosen?.task)!
+            self.dueDate = (theChosen?.dueDate)! as Date
+            self.priority = (theChosen?.priority)!
+            self.details = (theChosen?.details)!
+
+        }
+
+        
+        
+        
         
         navigationOptions = RowNavigationOptions.Enabled.union(.StopDisabledRow)
         animateScroll = true
@@ -53,7 +70,7 @@ class EntryFormViewController: FormViewController {
                 $0.onChange{[unowned self] row in
                     self.taskSubject = row.value!
                 }
-             
+                
                 
         }
         form +++ Section()
@@ -73,64 +90,71 @@ class EntryFormViewController: FormViewController {
                 $0.title = "Priority"
                 $0.options = ["L", "M", "H"]
                 $0.value = priority
+                $0.onChange({[unowned self] row in
+                    if let option = row.value{
+                        self.priority = option
+                    }})
             }
             +++ Section()
             <<< TextAreaRow(){
                 $0.placeholder = "Details"
                 $0.value = details
-                
+                $0.onChange({[unowned self] row in
+                    if let typedDetails = row.value{
+                        self.details = typedDetails
+                    }})
                 
         }
-
-       
+        
+        
     }
     /*
-    func setupForm(){
-        
-        form +++ Section("ABC")
-            <<< TextRow(){
-                $0.title = " Task "
-                $0.placeholder = "e.g Interview"
-                $0.value = taskSubject
-                $0.onChange{[unowned self] row in
-                    self.taskSubject = row.value!
-                }
-                
-        }
-        form +++ Section()
-            <<< DateTimeRow(){
-               
-                $0.title = "Due Date"
-                $0.dateFormatter = type(of: self).dateFormatter
-                $0.minimumDate = Date()
-                $0.value = dueDate
-                $0.onChange({[unowned self] row in
-                    if let date = row.value{
-                        self.dueDate = date
-                        
-                    }})
-            }
-            <<< SegmentedRow<String>() {
-                $0.title = "Priority"
-                $0.options = ["L", "M", "H"]
-                $0.value = priority
-            }
-            +++ Section()
-            <<< TextAreaRow(){
-                $0.placeholder = "Details"
-                $0.value = details
-                
-                
-        }
-    }
-    */
-  
-   static let dateFormatter: DateFormatter = {
+     func setupForm(){
+     
+     form +++ Section("ABC")
+     <<< TextRow(){
+     $0.title = " Task "
+     $0.placeholder = "e.g Interview"
+     $0.value = taskSubject
+     $0.onChange{[unowned self] row in
+     self.taskSubject = row.value!
+     }
+     
+     }
+     form +++ Section()
+     <<< DateTimeRow(){
+     
+     $0.title = "Due Date"
+     $0.dateFormatter = type(of: self).dateFormatter
+     $0.minimumDate = Date()
+     $0.value = dueDate
+     $0.onChange({[unowned self] row in
+     if let date = row.value{
+     self.dueDate = date
+     
+     }})
+     }
+     <<< SegmentedRow<String>() {
+     $0.title = "Priority"
+     $0.options = ["L", "M", "H"]
+     $0.value = priority
+     }
+     +++ Section()
+     <<< TextAreaRow(){
+     $0.placeholder = "Details"
+     $0.value = details
+     
+     
+     }
+     }
+     */
+    
+    static let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "MMM d yyyy, h mm:a"
         return formatter
     }()
     
- 
+    
     
 }
